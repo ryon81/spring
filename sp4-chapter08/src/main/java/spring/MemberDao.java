869d356c8c12c2a09841805ed4e8196bde6b7a33
ryon81 <ryon81@naver.com>
2016-12-29 11:52:39 +0900

    class 수업내용 업데이트

--- sp4-chapter08/src/main/java/spring/MemberDao.java
+++ sp4-chapter08/src/main/java/spring/MemberDao.java
@@ -1,33 +1,96 @@
 package spring;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
 
-public class MemberDao 
-{
-	private static long nextId = 0;
-	
-	private Map<String, Member> map = new HashMap<>();
-	
-	public Collection<Member> selectAll()
-	{
-		return null;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+public class MemberDao {
+
+	private JdbcTemplate jdbcTemplate;
+
+	public MemberDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-	
-	public Member selectByEmail(String email)
-	{
-		return null;
+
+	public Member selectByEmail(String email) {
+		List<Member> results = jdbcTemplate.query(
+				"select * from MEMBER where EMAIL = ?",
+				new RowMapper<Member>() {
+					@Override
+					public Member mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						Member member = new Member(rs.getString("EMAIL"),								
+								rs.getString("PASSWORD"),
+								rs.getString("NAME"),
+								rs.getTimestamp("REGDATE"));
+						member.setId(rs.getLong("ID"));
+						return member;
+					}
+				}
+				,email
+				);
+
+		return results.isEmpty() ? null : results.get(0);
 	}
-	
-	public void insert(Member member)
-	{
-		
+
+	public void insert(final Member member) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) 
+					throws SQLException {
+				PreparedStatement pstmt = con.prepareStatement(
+						"insert into MEMBER (ID, EMAIL, PASSWORD, NAME, REGDATE) "+
+						"values (id_num.nextval, ?, ?, ?, ?)",
+						new String[] {"ID"});
+				pstmt.setString(1,  member.getEmail());
+				pstmt.setString(2,  member.getPassword());
+				pstmt.setString(3,  member.getName());
+				pstmt.setTimestamp(4,  
+						new Timestamp(member.getRegisterDate().getTime()));
+				return pstmt;
+			}
+		}, keyHolder);
+		Number keyValue = keyHolder.getKey();
+		member.setId(keyValue.longValue());
 	}
-	
-	public void update(Member member)
-	{
-		
+
+	public void update(Member member) {
+		jdbcTemplate.update("update MEMBER set NAME = ?, PASSWORD = ? where EMAIL = ?",
+				member.getName(), member.getPassword(), member.getEmail());
+	}
+
+	public List<Member> selectAll() {
+		List<Member> results = jdbcTemplate.query("select * from MEMBER",
+				new RowMapper<Member>() {
+					@Override
+					public Member mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						Member member = new Member(rs.getString("EMAIL"),
+								rs.getString("PASSWORD"),
+								rs.getString("NAME"),
+								rs.getTimestamp("REGDATE"));
+						member.setId(rs.getLong("ID"));
+						return member;
+					}
+				});
+		return results;
+	}
+
+	public int count() {
+		Integer count = jdbcTemplate.queryForObject("select count(*) from MEMBER", Integer.class);
+		return count;
 	}
 
 }
